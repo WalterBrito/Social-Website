302ba3ab1b365eb2b58599fc9fffca7cde6a0582
WalterBrito <waltter.britto@gmail.com>
2016-07-11 20:28:50 -0300

    Social website made in django

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
     url('social-auth/', include('social.apps.django_app.urls', namespace='social')),
-
 ]
 
 if settings.DEBUG:
